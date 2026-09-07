1cf5b025aebd5aad7ac64339b4c0f36636eba2f9
edu-halaqat <abdulkareem20112011@gmail.com>
2026-09-07 17:00:11 +0300

    release(tallam): publish required signature and optional recommendations fix

--- tallam/assets/js/bootstrap.js
+++ tallam/assets/js/bootstrap.js
@@ -1,7 +1,7 @@
 "use strict";
 (async () => {
   const CACHE_COMPAT_BUILD = "20260831-mobile-provider-v4";
-  const RELEASE = "20260907-mobile-signature-optional-recommendations-v5";
+  const RELEASE = "20260907-mobile-signature-required-recommendations-optional-v6";
   const appMount = document.getElementById("appMount");
   window.__TALLAM_BUILD__ = RELEASE;
   document.body.dataset.portalReady = "loading";
